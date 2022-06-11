c51ab86d6c0a4b4ead68f4ce648be1c4e2fd5263
Jonas Silva <55038425+JonasCSilva@users.noreply.github.com>
2022-06-11 01:04:18 -0300

    Routing working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataComponent } from './data/data.component';
+import { InvComponent } from './inv/inv.component';
+import { MapComponent } from './map/map.component';
+import { RadioComponent } from './radio/radio.component';
+import { StatComponent } from './stat/stat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'stat', component: StatComponent },
+  { path: 'inv', component: InvComponent },
+  { path: 'data', component: DataComponent },
+  { path: 'map', component: MapComponent },
+  { path: 'radio', component: RadioComponent },
+  { path: '', redirectTo: '/stat', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StatComponent } from './stat/stat.component';
+import { InvComponent } from './inv/inv.component';
+import { DataComponent } from './data/data.component';
+import { MapComponent } from './map/map.component';
+import { RadioComponent } from './radio/radio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatComponent,
+    InvComponent,
+    DataComponent,
+    MapComponent,
+    RadioComponent
   ],
   imports: [
     BrowserModule,
