17edc4d38496561fe71c8b1cd24518548bef749c
Jonas Silva <55038425+JonasCSilva@users.noreply.github.com>
2022-06-23 22:14:04 -0300

    using routerLinkActive directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,16 @@
-import { Component } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { filter } from 'rxjs';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'pip-boy';
-  pagesNames = ['stat', 'inv', 'data', 'map', 'radio'];
-  selectedPageName!: string;
+  title = "pip-boy";
+  pagesNames = ["stat", "inv", "data", "map", "radio"];
 
   constructor(private router: Router) {}
-
-  ngOnInit() {
-    this.router.events
-      .pipe(filter((event) => event instanceof NavigationEnd))
-      .subscribe(({ urlAfterRedirects }: any) => {
-        this.selectedPageName = (urlAfterRedirects as string).split('/')[1];
-        /* .getBoundingClientRect() */
-      });
-  }
 
   doSomething(event: MouseEvent) {
     const element = event.target as HTMLAnchorElement;
