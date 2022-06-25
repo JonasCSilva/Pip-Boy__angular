da62326f03a50eca770f920213bf8a3e1b505770
Jonas Silva <55038425+JonasCSilva@users.noreply.github.com>
2022-06-25 18:41:19 -0300

    STAT Subheader working

--- src/app/subheader/subheader.component.ts
+++ src/app/subheader/subheader.component.ts
@@ -8,10 +8,10 @@ import {
   ViewChild,
   ViewChildren
 } from '@angular/core'
-import { ActivatedRoute /*, NavigationEnd, Router */ } from '@angular/router'
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router'
+import { filter } from 'rxjs'
 
 import { ElementsService } from '../services/elements.service'
-// import { filter } from 'rxjs'
 
 @Component({
   selector: 'app-subheader',
@@ -24,20 +24,28 @@ export class SubheaderComponent implements OnInit, AfterViewInit {
 
   subPagesNames: string[] = []
   widths: number[] = []
+  index!: number
   newLeft!: string
+  url!: string
 
   constructor(
     private route: ActivatedRoute,
-    /* private router: Router, */ private elementsService: ElementsService,
+    private router: Router,
+    private elementsService: ElementsService,
     private cd: ChangeDetectorRef
   ) {}
 
   ngOnInit() {
     this.subPagesNames = Object.values(this.route.snapshot.data)
 
-    /* this.router.events
-    .pipe(filter(event => event instanceof NavigationEnd))
-    .subscribe((data: any) => console.log(data.urlAfterRedirects)) */
+    this.url = this.router.url.split('/').pop()!
+    this.customFind()
+
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(({ urlAfterRedirects }: any) => {
+      this.url = urlAfterRedirects.split('/').pop()!
+      this.customFind()
+      this.updateLeft()
+    })
   }
 
   ngAfterViewInit() {
@@ -45,9 +53,22 @@ export class SubheaderComponent implements OnInit, AfterViewInit {
     for (const element of elements) {
       this.widths.push(element.nativeElement.getBoundingClientRect().width)
     }
-    const temp = this.elementsService.lefts[0] - this.widths[0] - 16 - this.widths[1] / 2
-    this.newLeft = temp + 'px'
+
+    this.updateLeft()
 
     this.cd.detectChanges()
+  }
+
+  customFind() {
+    this.index = this.subPagesNames.findIndex(value => value === this.url)
+  }
+
+  updateLeft() {
+    let aux = 0
+    for (let i = 0; i < this.index; i++) {
+      aux += this.widths[i] + 16
+    }
+    const temp = this.elementsService.lefts[0] - aux - this.widths[this.index] / 2
+    this.newLeft = temp + 'px'
   }
 }
