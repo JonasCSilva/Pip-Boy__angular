264f94abb5889f961fa022f78bd16582d01a0708
Jonas Silva <55038425+JonasCSilva@users.noreply.github.com>
2022-06-25 12:58:41 -0300

    Using pipes

--- src/app/subheader/subheader.component.ts
+++ src/app/subheader/subheader.component.ts
@@ -1,29 +1,30 @@
-import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { filter, map, Observable, take } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
-import {} from '@angular/router';
+import { Component, OnInit, QueryList, ViewChildren } from "@angular/core";
+import { Router, NavigationEnd } from "@angular/router";
+import { filter, map, Observable, take } from "rxjs";
+import { ActivatedRoute } from "@angular/router";
+import {} from "@angular/router";
 
 @Component({
-  selector: 'app-subheader',
-  templateUrl: './subheader.component.html',
-  styleUrls: ['./subheader.component.scss'],
+  selector: "app-subheader",
+  templateUrl: "./subheader.component.html",
+  styleUrls: ["./subheader.component.scss"],
 })
 export class SubheaderComponent implements OnInit {
-  @ViewChildren('cmp') components!: QueryList<HTMLAnchorElement>;
+  @ViewChildren("cmp") components!: QueryList<HTMLAnchorElement>;
 
   subPagesNames: string[] = [];
   selectedSubPageName!: string;
 
   constructor(private router: Router, route: ActivatedRoute) {
     const test = route.data.pipe(take(1));
+    console.log(route.snapshot.data);
 
     test.subscribe((x) => {
       this.subPagesNames = Object.values(x);
     });
 
     const url: Observable<string> = route.url.pipe(
-      map((segments) => segments.join(''))
+      map((segments) => segments.join(""))
     );
   }
 
