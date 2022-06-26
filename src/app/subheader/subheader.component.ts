bccbd1dfc0c756d17e769b7599aaafaa9b563510
Jonas Silva <55038425+JonasCSilva@users.noreply.github.com>
2022-06-25 22:14:12 -0300

    Subheader left fixed

--- src/app/subheader/subheader.component.ts
+++ src/app/subheader/subheader.component.ts
@@ -13,6 +13,8 @@ import { filter } from 'rxjs'
 
 import { ElementsService } from '../services/elements.service'
 
+const pagesWithSubHeader = ['stat', 'inv', 'data']
+
 @Component({
   selector: 'app-subheader',
   templateUrl: './subheader.component.html',
@@ -25,6 +27,7 @@ export class SubheaderComponent implements OnInit, AfterViewInit {
   subPagesNames: string[] = []
   widths: number[] = []
   index!: number
+  index2!: number
   newLeft!: string
   url!: string
 
@@ -38,7 +41,12 @@ export class SubheaderComponent implements OnInit, AfterViewInit {
   ngOnInit() {
     this.subPagesNames = Object.values(this.route.snapshot.data)
 
-    this.url = this.router.url.split('/').pop()!
+    const paths = this.router.url.split('/')
+
+    this.url = paths.pop()!
+    const path2 = paths.pop()!
+    this.index2 = pagesWithSubHeader.findIndex(page => page === path2)
+
     this.customFind()
 
     this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(({ urlAfterRedirects }: any) => {
@@ -68,7 +76,7 @@ export class SubheaderComponent implements OnInit, AfterViewInit {
     for (let i = 0; i < this.index; i++) {
       aux += this.widths[i] + 16
     }
-    const temp = this.elementsService.lefts[2] - 16 - aux - this.widths[this.index] / 2
+    const temp = this.elementsService.lefts[this.index2] - 16 - aux - this.widths[this.index] / 2
     this.newLeft = temp + 'px'
   }
 }
